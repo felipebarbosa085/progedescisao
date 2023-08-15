4dba27293868917fd7791d7056a9c0e648e97508
Felipe Barbosa <felipebarbosadosreis092@gmail.com>
2023-08-15 08:17:29 -0300

    Initial commit

---
+++ desvio_condicional_encadeado.elif.py
@@ -0,0 +1,13 @@
+'''
+Crie um programa que pergunte um número ao usuário. Em seguida o programa deve
+informar se o número inserido está abaixo de 100, entre 100 e 200 ou acima de 200.
+'''
+
+num = int(input("Informe um número: "))
+
+if (num < 100):
+    print(f"{num} está abaixo de 100")
+elif (num <= 200):
+    print(f"{num} está entre 100 e 200.")
+else:
+    print(f"{num} está acima de 200")
---
+++ desvio_condicional_simples.py
@@ -0,0 +1,14 @@
+'''
+Crie um programa que pergunte dois números ao usuário.
+Em seguida o programa deverá somar os dois números e apresentar o
+resultado se o valor for maior que 10.
+'''
+num1 = int(input("Informe um número: "))
+num2 = int(input("Informe outro número: "))
+
+soma = num1 + num2
+
+if(soma > 10):
+    print(f"A soma dos valores inseridos é {soma}")
+
+print("FIM DO PROGRAMA")
